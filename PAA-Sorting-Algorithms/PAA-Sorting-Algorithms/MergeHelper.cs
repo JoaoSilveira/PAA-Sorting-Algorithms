8a063b159ce2dc2627312a86d3cff677d5c76a97
João Pedro Silveira <joao.pedro0842@hotmail.com>
2017-07-06 22:52:37 -0300

    Fix MergeSort, Print order type

--- PAA-Sorting-Algorithms/PAA-Sorting-Algorithms/MergeHelper.cs
+++ PAA-Sorting-Algorithms/PAA-Sorting-Algorithms/MergeHelper.cs
@@ -1,3 +1,4 @@
+//#define ARRAY_COPY
 using System;
 
 namespace PAA_Sorting_Algorithms
@@ -6,66 +7,43 @@ namespace PAA_Sorting_Algorithms
     {
         public static void Sort(ref T[] collection, int init, int end, ref SortStatistics rep)
         {
+            if (end - init < 2)
+                return;
+
             var middle = (init + end) / 2;
 
-            if (end - init > 1)
-            {
-                Sort(ref collection, init, middle, ref rep);
-                Sort(ref collection, middle + 1, end, ref rep);
-            }
+            Sort(ref collection, init, middle, ref rep);
+            Sort(ref collection, middle, end, ref rep);
 
-            var arr = new T[end - init];
-            var left = init;
+            var aux = new T[middle - init];
+
+            Array.Copy(collection, init, aux, 0, aux.Length);
+
+            var left = 0;
             var right = middle;
 
-            for (var i = 0; i < arr.Length; i++)
+            int pos;
+
+            for (pos = init; left != aux.Length && right != end; pos++)
             {
-                if (collection[left].CompareTo(collection[right]) < 0)
+                ++rep.Comparations;
+                ++rep.Swaps;
+                if (aux[left].CompareTo(collection[right]) < 0)
                 {
-                    arr[i] = collection[left];
+                    collection[pos] = aux[left];
                     ++left;
                 }
                 else
                 {
-                    arr[i] = collection[right];
+                    collection[pos] = collection[right];
                     ++right;
                 }
-                
-                if (left == middle)
-                {
-                    for (var j = right; j < end; j++, i++)
-                        arr[i] = collection[j];
-                    break;
-                }
-
-                if (right != end) continue;
-
-                for (var j = left; j < middle; j++, i++)
-                    arr[i] = collection[j];
-                break;
             }
 
-            Array.Copy(arr, 0, collection, init, arr.Length);
-            //for (var i = init + 1; i < end; i++)
-            //{
-            //    var entry = collection[i];
-            //    var j = i;
-
-            //    while (collection[j - 1].CompareTo(entry) > 0)
-            //    {
-            //        ++rep.Comparations;
-
-            //        ++rep.Swaps;
-            //        collection[j] = collection[--j];
-
-            //        if (j != init) continue;
-
-            //        --rep.Comparations;
-            //        break;
-            //    }
-            //    ++rep.Comparations;
-            //    collection[j] = entry;
-            //}
+            if (right == end)
+            {
+                Array.Copy(aux, left, collection, pos, aux.Length - left);
+            }
         }
     }
 }
--- PAA-Sorting-Algorithms/PAA-Sorting-Algorithms/Program.cs
+++ PAA-Sorting-Algorithms/PAA-Sorting-Algorithms/Program.cs
@@ -29,43 +29,26 @@ namespace PAA_Sorting_Algorithms
 
         static void Main(string[] args)
         {
-            //if (args.Length != 1)
-            //{
-            //    Console.WriteLine("Write the path to the data directory");
-            //    return;
-            //}
-
-            foreach (var file in Directory.GetFiles(@"C:\Users\Malaquias\Source\Repos\PAA-Sorting-Algorithms\PAA-Sorting-Algorithms\PAA-Sorting-Algorithms\Data"))
+            if (args.Length != 1)
             {
-                var arr = File.ReadAllLines(file).Select(q => Convert.ToInt32(q)).ToArray();
-
-                var rep = SortingTypes<int>.MergeSort(ref arr);
-
-                for (var i = 0; i < arr.Length - 1; i++)
-                {
-                    if (arr[i] > arr[i + 1])
-                    {
-                        Console.WriteLine("Deu ruim");
-                        break;
-                    }
-                }
-                Console.WriteLine($"{Path.GetFileNameWithoutExtension(file)} || {rep.Comparations} - {rep.Swaps} - {rep.Time}");
+                Console.WriteLine("Write the path to the data directory");
+                return;
             }
 
-            //Thread.CurrentThread.Priority = ThreadPriority.Highest;
-            //Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime;
-            //foreach (var filePath in Directory.GetFiles(args[0]))
-            //{
-            //    using (var file = File.OpenRead(filePath))
-            //    {
-            //        var match = Regex.Match(Path.GetFileNameWithoutExtension(filePath),
-            //            @"^(?<order_type>[a-zA-Z]+)(?<vector_length>\d+)$");
-            //        var orderType = match.Groups["order_type"].Value;
-            //        var vectorLength = Convert.ToInt32(match.Groups["vector_length"].Value);
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime;
+            foreach (var filePath in Directory.GetFiles(args[0]))
+            {
+                using (var file = File.OpenRead(filePath))
+                {
+                    var match = Regex.Match(Path.GetFileNameWithoutExtension(filePath),
+                        @"^(?<order_type>[a-zA-Z]+)(?<vector_length>\d+)$");
+                    var orderType = match.Groups["order_type"].Value;
+                    var vectorLength = Convert.ToInt32(match.Groups["vector_length"].Value);
 
-            //        RunFile(file, Order[orderType], vectorLength);
-            //    }
-            //}
+                    RunFile(file, Order[orderType], vectorLength);
+                }
+            }
         }
 
         private static void RunFile(Stream file, string order, int vectorLength)
@@ -86,7 +69,7 @@ namespace PAA_Sorting_Algorithms
                 var copy = new int[vectorLength];
                 Array.Copy(originalVector, copy, vectorLength);
 
-                Console.WriteLine("{0} {1} - Started at {2}", rep.Algorithm, rep.VectorSize, DateTime.Now);
+                Console.WriteLine("{0} {1} {2} - Started at {3}", rep.Algorithm, rep.VectorSize, order, DateTime.Now);
 
                 rep.Statistics = (SortStatistics)methodInfo.Invoke(null, new object[] { copy });
 
